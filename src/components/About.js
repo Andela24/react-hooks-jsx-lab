54ee270ddfab566d384848cc5eb68dd7e2d5ba70
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-04 17:36:27 -0500

    commit tree 63bbec76d00298297a3d8bda6cdaed9c62a66826

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id = "about">
+    <h2>About Me</h2>
+    <p> The world is bigger than you think it is and your worries aren't as important as you think they are, just be you!</p>
+    <img 
+    src = {image}
+    alt="I made this"></img>
+  </div>;
 }
 
 export default About;
